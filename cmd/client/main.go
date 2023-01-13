f0ab84ae538089dd54676bc7a35192fa4d8ce3c1
Jonathan <chil.tfm@gmail.com>
2023-01-13 15:51:33 +0100

    feat: create main for client and server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 1 {
+		log.Fatal("Usage: go run main.go")
+	}
+	/*cl := client.Client()
+	cl.Run()*/
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	errorMessage := "Usage: go run main.go <server number>"
+	if len(os.Args) != 2 {
+		log.Fatal(errorMessage)
+	}
+
+	number, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(errorMessage)
+	} else if number < 0 || number > 5 { // todo ne pas hardcoder le 5
+		log.Fatal("Number must be between 0 and 5")
+	}
+
+	/*
+		server := server.Server(number)
+		server.Run()
+	*/
+}
