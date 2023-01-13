714ace53a23b47f1482d8d8247f723e0d4c25a7e
Lazzzer <dev@lazarpavicevic.ch>
2023-01-13 15:56:12 +0100

    feat: add parser

---
+++ internal/shared/types/types.go
@@ -0,0 +1,40 @@
+package types
+
+// Config représente la configuration du réseau de serveurs.
+type Config struct {
+	Servers map[int]string `json:"servers"` // Liste des adresse des serveurs disponibles
+}
+
+type ServerConfig struct {
+	Servers       map[int]Server `json:"servers"`        // Liste des serveurs disponibles avec leur lettre et leur adresse
+	AdjacencyList map[int][]int  `json:"adjacency_list"` // Liste d'adjacence des serveurs
+}
+
+type Server struct {
+	Letter  string `json:"letter"`  // Lettre à compter
+	Address string `json:"address"` // Adresse du serveur
+}
+
+type LogType string // Type de log
+
+const (
+	INFO    LogType = "INFO"    // Log d'information
+	DEBUG   LogType = "DEBUG"   // Log de debug
+	ERROR   LogType = "ERROR"   // Log d'erreur
+	COMMAND LogType = "COMMAND" // Log de commande
+)
+
+type CommandType string // Type de commande
+
+const (
+	Count CommandType = "count" // Commande de comptage des occurrences de lettres
+	Ask   CommandType = "ask"   // Commande de demande du résultat d'un comptage sur un mot
+
+)
+
+// Command représente une commande envoyée par un client.
+type Command struct {
+	Type   CommandType `json:"command_type"`    // Type de la commande
+	Text   string      `json:"text,omitempty"`  // Texte à analyser
+	Server *int        `json:"value,omitempty"` // Nb de serveurs à utiliser
+}
--- internal/shared/util.go
+++ internal/shared/util.go
@@ -1,1 +1,46 @@
 package shared
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/Lazzzer/labo4-sdr/internal/shared/types"
+)
+
+// Parse permet de parser un objet JSON en un objet de type T.
+func Parse[T types.Config | types.Command](jsonStr string) (*T, error) {
+	var object T
+
+	err := json.Unmarshal([]byte(jsonStr), &object)
+	if err != nil {
+		return nil, err
+	}
+
+	return &object, nil
+}
+
+// Log permet d'afficher un message dans la console avec une couleur différente selon le type de log.
+func Log(logType types.LogType, message string) {
+	switch logType {
+	case types.INFO:
+		log.Println(CYAN + "(INFO) " + RESET + message)
+	case types.DEBUG:
+		log.Println(ORANGE + "(DEBUG) " + RESET + message)
+	case types.ERROR:
+		log.Println(RED + "(ERROR) " + RESET + message)
+	case types.COMMAND:
+		log.Println(YELLOW + "(COMMAND) " + RESET + message)
+	}
+}
+
+// Variables pour colorer le texte dans la console
+var RESET = "\033[0m"         // Variable pour réinitialiser la couleur du texte
+var RED = "\033[31m"          // Variable pour colorer le texte en rouge
+var PINK = "\033[38;5;219m"   // Variable pour colorer le texte en rose
+var PURPLE = "\033[38;5;198m" // Variable pour colorer le texte en violet
+
+var GREEN = "\033[32m"        // Variable pour colorer le texte en vert
+var YELLOW = "\033[33m"       // Variable pour colorer le texte en jaune
+var ORANGE = "\033[38;5;208m" // Variable pour colorer le texte en orange
+var CYAN = "\033[36m"         // Variable pour colorer le texte en cyan
+var BOLD = "\033[1m"          // Variable pour changer le texte en gras
